81347e8b6c14b0bc9f2bdb538e78061265729751
Gregory Rehm <grehm87@gmail.com>
2020-12-10 02:09:43 -0800

    add initial setup stuff

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(name='parliament',
+      version="1.0",
+      description='Single chamber algorithms for resistance/compliance models. Also includes data and benchmarking suite.',
+      packages=find_packages(),
+      install_requires=[
+      ],
+      entry_points={
+      },
+      )
