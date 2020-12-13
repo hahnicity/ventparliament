70866e6852e57bbb95fadad52d015386bb55b8d5
Gregory Rehm <grehm87@gmail.com>
2020-12-12 20:25:43 -0800

    add updates to comments on CO algo

--- parliament/vicario_constrained.py
+++ parliament/vicario_constrained.py
@@ -15,6 +15,8 @@ from scipy.optimize import fmin_slsqp
 
 def construct_U(flow, vols):
     """
+    This is eq 8a in the paper, with some minor simplifications.
+
     This works by creating a 2 dim matrix (N+2,N+2) in size. provided we multiply this using
     x.dot(U).dot(x), the upper left resolves out to be: R^2*sum(V_dot^2) + 2REsum(V_dotV)
     + E^2sum(V^2). Then with the way we setup the matrix the rest of the multiplications resolve
@@ -45,14 +47,14 @@ def construct_bound_mats_ineq(n_obs, x0_idx, m_idx):
     # We use +2 on the number of columns because we are also optimizing for elastance and resistance
     bm_ineq = np.zeros([x0_idx-1, n_obs+2])
 
-    # 3a. this stands for P_mus(k+1) - P_mus(k) <= 0  k=1,2,...,m-1
+    # 5a. this stands for P_mus(k+1) - P_mus(k) <= 0  k=1,2,...,m-1
     # which is converted to -P_mus(k+1) + P_mus(k) >= 0, because
-    # fmin_slsq does not utilize negative inequalities
+    # fmin_slsq utilizes positive inequalities
     for i in range(0, m_idx):
         bm_ineq[i][i+2] = 1
         bm_ineq[i][i+3] = -1
 
-    # 3b. this stands for P_mus(k+1) - P_mus(k) >= 0  k=m,m+1,...,q-1
+    # 5b. this stands for P_mus(k+1) - P_mus(k) >= 0  k=m,m+1,...,q-1
     for i in range(m_idx, x0_idx-1):
         bm_ineq[i][i+2] = -1
         bm_ineq[i][i+3] = 1
@@ -62,7 +64,7 @@ def construct_bound_mats_ineq(n_obs, x0_idx, m_idx):
 def construct_bound_mats(n_obs, x0_idx, m_idx):
     """
     Construct our constraining matrix A for our inequalities and
-    equalities defined in 3a, 3b, and 3c.
+    equalities defined in 5a, 5b, and 5c.
     """
     bm_ineq = construct_bound_mats_ineq(n_obs, x0_idx, m_idx)
     bm_eq = np.zeros([n_obs-x0_idx-1, n_obs+2])
@@ -79,9 +81,10 @@ def objective(S, T, U, x):
 
 
 def _setup_constrained_optimization(flow, vols, pressure, x0, m_idx, r_max, r_min, e_max, e_min, p_min, p_max, initial_guess):
+
     if isinstance(initial_guess, type(None)):
         initial_guess = np.random.rand(len(flow)+2)
-    # all code come from the optimization of eq 2. in the paper
+    # all code come from the optimization of eq 4. in the paper
     #
     # This is just sum(P_ao(k)^2), just call this S
     S = np.sum(pressure ** 2)
@@ -96,6 +99,8 @@ def _setup_constrained_optimization(flow, vols, pressure, x0, m_idx, r_max, r_mi
     ieq_con = lambda p_mus: bm_ineq.dot(p_mus)
     eq_con = lambda p_mus: bm_eq.dot(p_mus)
     return obj, initial_guess, bounds, ieq_con, eq_con
+
+
 
 
 def perform_constrained_optimization(flow, vols, pressure, x0, m_idx, r_max=100, r_min=0, e_max=100, e_min=0, p_min=-15, p_max=20, initial_guess=None):
@@ -120,7 +125,7 @@ def perform_constrained_optimization(flow, vols, pressure, x0, m_idx, r_max=100,
     )
     # this can get an error "x0 violates bound constraints if you have scipy 1.5.X. So as a result,
     # we've set scipy=1.4.1 in environment.yml
-    result = fmin_slsqp(obj, initial_guess, eqcons=[eq_con], ieqcons=[ieq_con], bounds=bounds)
+    result = fmin_slsqp(obj, initial_guess, f_eqcons=eq_con, f_ieqcons=ieq_con, bounds=bounds)
     R = result[0]
     E = result[1]
     P_mus = result[2:]
@@ -149,7 +154,7 @@ def optimize_insp_lim_only(flow, vols, pressure, x0, m_idx, r_max=100, r_min=0, 
     obj, initial_guess, bounds, ieq_con, eq_con = _setup_constrained_optimization(
         flow, vols, pressure, x0, m_idx, r_max, r_min, e_max, e_min, p_min, p_max, initial_guess
     )
-    result = fmin_slsqp(obj, initial_guess, ieqcons=[ieq_con], bounds=bounds)
+    result = fmin_slsqp(obj, initial_guess, f_ieqcons=ieq_con, bounds=bounds)
     R = result[0]
     E = result[1]
     P_mus = result[2:]
